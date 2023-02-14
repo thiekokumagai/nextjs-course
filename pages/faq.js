02a671eb57f24746171db3a52d9cd3a7b5b7eb06
thiekokumagai <thieko.kumagai@gmail.com>
2023-02-14 13:15:53 -0400

    Primeiro commit

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage(){
+    return(
+        <div>
+            <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
+            <a href="/">Ir para o home</a>
+        </div>
+    )
+}
