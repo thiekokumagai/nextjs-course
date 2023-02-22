e42566e6f474cdba86ce8762fc61b48699d6c46b
thiekokumagai <thieko.kumagai@gmail.com>
2023-02-22 15:54:51 -0400

    commit

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,9 +1,10 @@
-import NextLink from 'next/link'
+import NextLink from 'next/link';
+import { Text } from '../../theme/components';
 
-export default function Link({children,href}){
+export default function Link({ children, href, ...props}) {
     return (
         <NextLink href={href} passHref>
-            {children}
+            <Text as="a" {...props}>{children}</Text>
         </NextLink>
-    )
+    );
 }
