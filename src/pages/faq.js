652a9ffa3faf87dc4a4d0d6f8dd4cc2ed4690eda
thiekokumagai <thieko.kumagai@gmail.com>
2023-02-22 15:14:28 -0400

    Publicar

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,5 +1,16 @@
+import { useEffect } from 'react'
 import Link from '../components/link'
 export default function FAQPage(){
+    useEffect(()=>{
+        const FAQ_API_URL = '';
+        fetch(FAQ_API_URL)
+            .then((respostaDoServidor)=>{
+                return respostaDoServidor.json();
+            })
+            .then((resposta) => {
+                console.log(resposta);
+            });
+    }, []);
     return(
         <div>
             <h1>Alura Cases - Pagina de Perguntas FAQ</h1>
